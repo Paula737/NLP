f6a833d1d3c83c46c85f3f2cd66240049aa84fd1
Paula Hanna <paulahanna700@gmail.com>
2026-08-15 19:30:38 +0300

    Delete week 5/ai_coding_assistant directory

--- week 5/ai_coding_assistant/test_explainer.py
+++
@@ -1,9 +0,0 @@
-# test_explainer.py
-from modules.explainer import explain_results
-
-result = explain_results(
-    question="what is the average salary in the engineering department",
-    sql="SELECT AVG(salary) FROM dataset WHERE LOWER(department) = LOWER('engineering')",
-    rows=[{"AVG(salary)": 8550}]
-)
-print(result)
--- week 5/ai_coding_assistant/test_transcriber.py
+++
@@ -1,5 +0,0 @@
-# test_transcriber.py
-from modules.transcriber import transcribe_audio
-
-result = transcribe_audio("Recording.m4a")
-print(result)
